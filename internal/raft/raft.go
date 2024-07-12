ce5eb4c5588af3eff3708402fe346223b36d3eea
Vismay-dev <vismaysuramwar@gmail.com>
2024-07-12 18:34:45 +0400

    leader election: appendEntry RPC

--- internal/raft/raft.go
+++ internal/raft/raft.go
@@ -2,11 +2,13 @@ package raft
 
 import (
 	"log"
+	"math/rand"
 	"net"
 	"net/http"
 	"net/rpc"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 type State string
@@ -24,6 +26,7 @@ type Raft struct {
 	votedFor    int
 	peers		[]string
 	shutdownCh  chan struct{}
+	timerCh 	chan struct{}		
 }
 
 func Make(peers []string, me int) {
@@ -37,6 +40,8 @@ func Make(peers []string, me int) {
 	}
 
 	rf.serve()
+
+	go rf.electionTimeout()
 }
 
 func (rf *Raft) serve() {
@@ -52,6 +57,71 @@ func (rf *Raft) serve() {
 	go http.Serve(listener, nil)
 }
 
+func (rf *Raft) electionTimeout() {
+	start := time.Now()
+	timeout := time.Duration(300 + rand.Intn(200)) * time.Millisecond
+	for {
+		select {
+		case <-rf.timerCh:
+			return
+		default:
+			now := time.Now()
+			elapsed := now.Sub(start)
+			if elapsed > timeout {
+				if rf.state == Candidate {
+					rf.state = Follower
+				}
+				go rf.startElection()
+				go rf.electionTimeout()
+				return
+			}
+		}		
+	}
+}
+
+func (rf *Raft) sendHeartbeats() {
+	rf.mu.Lock()
+	args := &AppendEntriesRequest{
+		Term: rf.currentTerm,
+	}
+	rf.mu.Unlock()
+	start := time.Now()
+	timeout := time.Duration(30) * time.Millisecond
+
+	for {
+		now := time.Now()
+		elapsed := now.Sub(start)
+		if elapsed >= timeout {
+
+			var wg sync.WaitGroup
+			for _, peer := range rf.peers {
+				wg.Add(1)
+				if peer != rf.peers[rf.me] {
+					go func(peer string) {
+						defer wg.Done()
+						var reply AppendEntriesResponse
+						if rf.sendAppendEntry(peer, args, &reply) {
+							rf.mu.Lock()
+							defer rf.mu.Unlock()
+							if reply.Term > rf.currentTerm && !reply.Success {
+								rf.state = Follower
+								rf.votedFor = -1
+							}
+						}
+					}(peer)
+				}
+			}
+
+			wg.Wait()
+			if rf.state == Follower {
+				go rf.electionTimeout()
+				return
+			}
+			start = now
+		}
+	}
+}
+
 func (rf *Raft) startElection() {
 	rf.mu.Lock()
 	rf.state = Candidate
@@ -64,15 +134,15 @@ func (rf *Raft) startElection() {
 	rf.mu.Unlock()
 
 	var votes	int32
-	var wg		sync.WaitGroup
+	var cond	sync.Cond
 
 	for _, peer := range rf.peers {
 		if peer != rf.peers[rf.me] {
-			wg.Add(1)
 			go func(peer string) {
-				defer wg.Done()
 				var reply RequestVoteResponse
 				if rf.sendRequestVote(peer, args, &reply) {
+					rf.mu.Lock()
+					defer rf.mu.Unlock()
 					if reply.Term > rf.currentTerm {
 						rf.currentTerm = reply.Term
 						rf.votedFor = -1
@@ -80,20 +150,36 @@ func (rf *Raft) startElection() {
 					} else if reply.Term == rf.currentTerm && reply.VoteGranted {
 						atomic.AddInt32(&votes, 1)
 					}
+					cond.Broadcast()
 				}
 			}(rf.peers[rf.me])
 		}
 	}
 
-	wg.Wait()
-	if rf.state == Candidate && votes > int32(len(rf.peers) / 2) {
-		rf.state = Leader
-		// start heartbeats goroutine
+	rf.mu.Lock()
+	for votes <= int32(len(rf.peers) / 2) {
+		cond.Wait()
+		if rf.state != Candidate {
+			rf.mu.Unlock()
+			return
+		}
 	}
-	
+
+	rf.timerCh <- struct{}{}
+	rf.state = Leader
+	go rf.sendHeartbeats()
+	rf.mu.Unlock()
+}
+
+func (rf *Raft) sendAppendEntry(peer string, req *AppendEntriesRequest, res *AppendEntriesResponse) bool {
+	return call(peer, "Raft.HandleAppendEntry", req, res)
 }
 
 func (rf *Raft) sendRequestVote(peer string, req *RequestVoteRequest, res *RequestVoteResponse) bool {
+	return call(peer, "Raft.HandleRequestVote", req, res)
+}
+
+func call(peer, rpcname string, req interface{}, res interface{}) bool {
 	conn, err := net.Dial("tcp", peer)
 	if err != nil {
 		log.Fatalf("Failed to dial %s: %s", peer, err)
@@ -101,12 +187,33 @@ func (rf *Raft) sendRequestVote(peer string, req *RequestVoteRequest, res *Reque
 	defer conn.Close()
 
 	client := rpc.NewClient(conn)
-	if err := client.Call("Raft.HandleRequestVote", req, &res); err != nil {
+	if err := client.Call(rpcname, req, &res); err != nil {
 		log.Fatalf("RPC call failed: %s", err)
 		return false
 	}
 
 	return true
+}
+
+func (rf *Raft) HandleAppendEntry(
+	appendEntryReq *AppendEntriesRequest,
+	appendEntryRes *AppendEntriesResponse,
+) error {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	if rf.currentTerm > appendEntryReq.Term {
+		appendEntryRes.Term = rf.currentTerm
+		appendEntryRes.Success = false
+		return nil
+	}
+
+	rf.currentTerm = appendEntryReq.Term
+	appendEntryRes.Term = rf.currentTerm
+	appendEntryRes.Success = true
+	rf.timerCh <- struct{}{}
+	go rf.electionTimeout()
+	return nil
 }
 
 func (rf *Raft) HandleRequestVote(
